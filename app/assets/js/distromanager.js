63ec574cf8a0bace7780b97be834897142b0d358
Tuna Kirkoc <tuna@kirkoc.fr>
2024-07-06 15:04:00 +0200

    Update distro URL

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -3,7 +3,7 @@ const { DistributionAPI } = require('helios-core/common')
 const ConfigManager = require('./configmanager')
 
 // Current Distro url.
-exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/Kirkoc/distribution/main/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://cdn.kirkoc.net/distribution/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
